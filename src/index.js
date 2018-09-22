fe732a9dd0cdca58b47500b8a45d5dc35b174647
mentos2805 <mentos28059@gmail.com>
2018-09-23 00:59:41 +0300

    done

--- src/index.js
+++ src/index.js
@@ -1,31 +1,41 @@
 class Sorter {
   constructor() {
-    // your implementation
+      this.arr = [];
+      this._cmp = function(a, b) {
+        return a - b;
+      };
+      this._length = 0;
   }
 
-  add(element) {
-    // your implementation
+  add(v) {
+      this.arr.push(v);
+      this._length++;
   }
-
-  at(index) {
-    // your implementation
+  sort(indexes) {
+      const values = [];
+      for (var i=0;i<indexes.length;i++) {
+          values.push(this.arr[indexes[i]]);
+      }
+      values.sort(this._cmp);
+      indexes.sort(function(a, b) {
+        return a - b;
+      });
+      for (var i=0;i<indexes.length;i++) {
+          this.arr[indexes[i]] = values[i];
+      }
   }
-
-  get length() {
-    // your implementation
-  }
-
   toArray() {
-    // your implementation
+      return this.arr;
+  }
+  setComparator(cmp) {
+      this._cmp = cmp;
+  }
+  get length() {
+    return this._length;
   }
 
-  sort(indices) {
-    // your implementation
-  }
-
-  setComparator(compareFunction) {
-    // your implementation
+  at(i){
+    return this.arr[i];
   }
 }
-
 module.exports = Sorter;
